4bfd7cb27cbd1040af87b8aa604239d30810138f
seiflafaf <seif.laflaf@gmail.com>
2026-02-17 18:15:08 +0100

    modifiche script per export con aggiunte delle esportazione

--- sql/05_EXPORT_DATA_ACN_CSV.sql
+++ sql/05_EXPORT_DATA_ACN_CSV.sql
@@ -10,6 +10,13 @@
 -- Sostituire 'C:/export/asset_critici_acn.csv' con un percorso
 -- valido e con permessi lettura/scrittura sul proprio computer. La cartella (es. C:/export/)
 -- deve esistere prima di eseguire lo script.
+-- EXPORT 1: Inventario Asset Critici
+\echo 'Esportazione 1: Inventario Asset Critici...'
 \copy (SELECT * FROM report_asset_critici_acn) TO 'C:\export\asset_critici_acn.csv' WITH CSV HEADER;
 
-\echo 'Esportazione completata. File creato in C:\export\asset_critici_acn.csv'
+-- EXPORT 2: Report Gap Analysis (Compliance)
+-- Questo file contiene il dettaglio Asset -> Controllo -> Esito
+\echo 'Esportazione 2: Report Gap Analysis (Compliance)...'
+\copy (SELECT * FROM report_gap_analysis_acn) TO 'C:\export\gap_analysis_acn.csv' WITH CSV HEADER;
+
+\echo 'Esportazione completata. File creati in C:\export\'
