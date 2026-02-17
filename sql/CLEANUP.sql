e46c775d4bc9f2089024029b6e77668ed96e90d4
seiflafaf <seif.laflaf@gmail.com>
2026-02-17 18:14:14 +0100

    modifica script di servizio

--- sql/CLEANUP.sql
+++ sql/CLEANUP.sql
@@ -5,17 +5,32 @@
 -- #################################################################
 \echo 'Inizio pulizia (DROP)...'
 
--- "IF EXISTS" assicura che lo script non fallisca se gli oggetti
--- sono già stati cancellati.
+-- VISTE
+DROP VIEW IF EXISTS report_gap_analysis_acn; 
 DROP VIEW IF EXISTS report_asset_critici_acn;
+
+-- TABELLE (Ordine Inverso rispetto alla creazione per vincoli FK)
 DROP TABLE IF EXISTS DIPENDENZA_TERZI;
 DROP TABLE IF EXISTS SERVIZIO_ASSET;
+
+-- Cancellazione tabelle Compliance/Framework
+-- Asset Compliance dipende da Asset e Sottocategoria, quindi va cancellata prima
+DROP TABLE IF EXISTS ASSET_COMPLIANCE; 
+
 DROP TABLE IF EXISTS ASSET_STORICO;
 DROP TABLE IF EXISTS ASSET;
 DROP TABLE IF EXISTS SERVIZIO;
 DROP TABLE IF EXISTS FORNITORE_TERZO;
 DROP TABLE IF EXISTS RESPONSABILE;
+
+-- Cancellazione tabelle Struttura ACN
+DROP TABLE IF EXISTS ACN_SOTTOCATEGORIA;
+DROP TABLE IF EXISTS ACN_CATEGORIA;
+DROP TABLE IF EXISTS ACN_FUNZIONE;
+
 DROP TABLE IF EXISTS AZIENDA;
+
+-- FUNZIONI
 DROP FUNCTION IF EXISTS log_asset_changes();
 
 \echo 'Pulizia completata.'
