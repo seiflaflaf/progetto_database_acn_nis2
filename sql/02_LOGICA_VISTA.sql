c60c9321fcabdd7d7ac4513cf48f6e5a05a9a420
seiflafaf <seif.laflaf@gmail.com>
2026-02-17 17:14:53 +0100

    modifica viste

--- sql/02_LOGICA_VISTA.sql
+++ sql/02_LOGICA_VISTA.sql
@@ -33,29 +33,37 @@ BEFORE UPDATE OR DELETE ON ASSET
 FOR EACH ROW EXECUTE PROCEDURE log_asset_changes();
 
 
--- VISTA (VIEW) per il report ACN degli asset critici
--- 'CREATE OR REPLACE' rende la creazione della vista idempotente.
+-- VISTA 1: Asset Critici (Esistente, utile per inventario rapido)
 CREATE OR REPLACE VIEW report_asset_critici_acn AS
 SELECT
     A.codice_asset AS "ID_Asset",
     A.tipo AS "Tipo_Asset",
-    A.descrizione AS "Descrizione_Asset",
-    A.criticita AS "Livello_Criticita'",
+    A.criticita AS "Livello_Criticita",
     S.nome_servizio AS "Servizio_Supportato",
-    R.nome || ' ' || R.cognome AS "Nome_Responsabile_Servizio",
-    R.email AS "Email_Contatto"
-FROM 
-    ASSET A
-INNER JOIN 
-    SERVIZIO_ASSET SA ON A.asset_id = SA.asset_id 
-INNER JOIN 
-    SERVIZIO S ON SA.servizio_id = S.servizio_id   
-INNER JOIN  
-    RESPONSABILE R ON S.responsabile_id = R.responsabile_id 
-WHERE
-    A.azienda_id = 1 
-    AND A.criticita IN ('Alta', 'Critica')
-ORDER BY
-    A.criticita DESC, A.codice_asset ASC;
+    R.email AS "Email_Responsabile"
+FROM ASSET A
+JOIN SERVIZIO_ASSET SA ON A.asset_id = SA.asset_id 
+JOIN SERVIZIO S ON SA.servizio_id = S.servizio_id   
+JOIN RESPONSABILE R ON S.responsabile_id = R.responsabile_id 
+WHERE A.azienda_id = 1 AND A.criticita IN ('Alta', 'Critica');
+
+-- VISTA 2: GAP ANALYSIS (Mostra lo stato di conformità per ogni asset rispetto alle sottocategorie ACN)
+CREATE OR REPLACE VIEW report_gap_analysis_acn AS
+SELECT 
+    A.nome_azienda,
+    ASS.codice_asset,
+    FUN.codice AS "Funzione",
+    CAT.codice AS "Categoria",
+    SUB.codice AS "Sottocategoria_ACN",
+    SUB.descrizione AS "Requisito",
+    COMP.stato_conformita,
+    COMP.note_audit
+FROM ASSET_COMPLIANCE COMP
+JOIN ASSET ASS ON COMP.asset_id = ASS.asset_id
+JOIN AZIENDA A ON ASS.azienda_id = A.azienda_id
+JOIN ACN_SOTTOCATEGORIA SUB ON COMP.sottocategoria_id = SUB.sottocategoria_id
+JOIN ACN_CATEGORIA CAT ON SUB.categoria_id = CAT.categoria_id
+JOIN ACN_FUNZIONE FUN ON CAT.funzione_id = FUN.funzione_id
+ORDER BY ASS.codice_asset, SUB.codice;
 
 \echo 'Trigger e Viste DB create.'
